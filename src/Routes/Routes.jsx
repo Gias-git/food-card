1416915798ced7e72ba6609107bf2b0b7c24f126
Gias-git <giasuddin.programmer@gmail.com>
2024-12-28 20:48:35 +0600

    first commit

---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {
+    createBrowserRouter,
+    RouterProvider,
+  } from "react-router-dom";
+
+export const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <div>Hello world!</div>,
+    },
+  ]);
+   
+
